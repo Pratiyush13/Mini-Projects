da1dae403bcf1e7023f480889c87938ff32b4e3d
Pratiyush Bipin Mahadik <pratiyushm333@gmail.com>
2024-12-29 17:15:36 +0530

    Add files via upload

---
+++ Rock,Paper Scissor Using React/src/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import './App.css';
+import Game from './Game.js';
+
+class App extends Component{  
+    render(){
+      return (
+      <Game/>
+    );
+  }
+}
+
+export default App;
